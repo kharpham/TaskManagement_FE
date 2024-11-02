a735014b01554b06619a1d6672fbde3de9ddd14c
Khapham <khar22102003@gmail.com>
2024-11-02 08:43:57 +0700

    Config app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { TaskEditComponent } from './tasks/task-edit/task-edit.component';
+import { TaskCreateComponent } from './tasks/task-create/task-create.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from './core/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'tasks', component: TaskListComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/new', component: TaskCreateComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/:id', component: TaskDetailComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/:id/edit', component: TaskEditComponent, canActivate: [AuthGuard] },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
+];
