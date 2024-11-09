9bf73996d4ba8781c9b15970715b1965504da240
Khapham <khar22102003@gmail.com>
2024-11-09 08:51:23 +0700

    Add token to task request

--- src/app/shared/services/task.service.ts
+++ src/app/shared/services/task.service.ts
@@ -1,3 +1,4 @@
+// src/app/shared/services/task.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -7,37 +8,40 @@ import { CookieService } from 'ngx-cookie-service';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'https://localhost:7090/api/task'; // Update this URL as needed
+  private apiUrl = 'https://localhost:7090/api/task';
 
   constructor(private http: HttpClient, private cookieService: CookieService) { }
 
-  getTasks(isComplete?: boolean): Observable<any[]> {
+  private getAuthHeaders(): HttpHeaders {
     const token = this.cookieService.get('jwt');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
+  getTasks(isComplete?: boolean): Observable<any[]> {
     let url = this.apiUrl;
     if (isComplete !== undefined) {
       url += `?isComplete=${isComplete}`;
     }
-    return this.http.get<any[]>(url, {
-      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`);
-    });
+    return this.http.get<any[]>(url, { headers: this.getAuthHeaders() });
   }
+
   getTask(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers: this.getAuthHeaders() });
   }
 
   createTask(task: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, task);
+    return this.http.post<any>(this.apiUrl, task, { headers: this.getAuthHeaders() });
   }
 
   updateTask(id: number, task: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, task);
-  }
-
-  updateTaskStatus(id: number, taskStatus: any): Observable<any> {
-    return this.http.patch<any>(`${this.apiUrl}/${id}/status`, taskStatus);
+    return this.http.put<any>(`${this.apiUrl}/${id}`, task, { headers: this.getAuthHeaders() });
   }
 
   deleteTask(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+    return this.http.delete<any>(`${this.apiUrl}/${id}`, { headers: this.getAuthHeaders() });
+  }
+
+  updateTaskStatus(id: number, status: any): Observable<any> {
+    return this.http.patch<any>(`${this.apiUrl}/${id}/status`, status, { headers: this.getAuthHeaders() });
   }
 }
